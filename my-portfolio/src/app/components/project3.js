24b8066ff63f739b405a0e9aa81cbf3c44a1c35a
RyanMdg <ryandeguia.dev@gmail.com>
2023-07-19 17:24:01 +0800

    fixing links

--- my-portfolio/src/app/components/project3.js
+++ my-portfolio/src/app/components/project3.js
@@ -7,7 +7,7 @@ const projects = () => {
     <div data-aos="fade-up" className="max-[1000px]:hidden mt-20">
       <div className=" relative grid grid-cols-2">
         <div class="relative overflow-hidden">
-          <a href="https://coachmardee.netlify.app/">
+          <a href="https://thegroceryhubmp2.vercel.app/">
             <img
               class=" duration-300 hover:scale-110 z-40 filter w-[100%] rounded-md"
               src="/groceryhub-mockup.png"
@@ -18,7 +18,7 @@ const projects = () => {
         <div className=" text-end me-10 mt-16">
           <span className=" text-[#ffd900]">Featured Project</span>
           <h1 className=" text-[2rem] font-bold text-[#CCD6F6]">
-            F.A Personal Website
+            Grocery Shop
           </h1>
           <p className=" py-3 px-5 pl-20 left-[32rem] right-10 mt-5  z-50 absolute text-[#8892B0] bg-[#112240] rounded-md shadow-lg hover:shadow-xl duration-300">
             The grocery hub website is a dynamic and user-friendly platform
